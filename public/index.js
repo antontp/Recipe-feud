41dce055df273379da97be24d973a67b219b53e1
AntonTinPhan-nm2207 <e1110167@u.nus.edu>
2023-03-03 02:28:39 +0800

    Trying to fix fetch data delay server

--- public/index.js
+++ public/index.js
@@ -1,21 +1,38 @@
-const api_url_dish = 'https://www.themealdb.com/api/json/v1/1/random.php';
-const api_url_ingredients = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
-const socket = io();
+// game data
+var ingredients = null;
+var dish = null;
 
-// Get player number from server
-socket.on("player-number", playerNum => {
-    if (!playerNum) console.log("No space, server full lah");
-    else console.log(`You got player number ${playerNum}`);
-})
-// player 1 == true
-// player 2 == false
-var playerSwitch = true;
+const feedbackEl = document.getElementById("feedback");
+const lobbyEl = document.getElementById("lobby");
+const ingredientsDisplayEl = document.getElementById("ingredientsDisplay");
 
-// Fetching DOM objects
-const containerEl = document.getElementById("container");
-const playerTurnEl = document.getElementById("playerTurn");
-const dishDisplayEl = document.getElementById("dishDisplay");
-const ingredientsDisplayEl = document.getElementById("ingredientsDisplay")
+function startClient() {
+    console.log("Hello?");
+    const socket = io();
+    // Get player number from server
+    socket.on("player-number", playerNum => {
+        if (!playerNum) {
+            console.log("Hidden");
+            feedbackEl.innerHTML = ("No space, server full lah");
+            lobbyEl.style.display = "none";
+        }
+        else console.log(`You got player number ${playerNum}`);
+    });
+
+    // Get game data from server
+    socket.on("game-data", (serverIngredients, serverDish) => {
+        console.log(serverIngredients);
+        console.log(serverDish);
+        // ingredients = serverIngredients;
+        // dish = serverDish;
+    });
+}
+
+// // Fetching DOM objects
+// const containerEl = document.getElementById("container");
+// const playerTurnEl = document.getElementById("playerTurn");
+// const dishDisplayEl = document.getElementById("dishDisplay");
+// const ingredientsDisplayEl = document.getElementById("ingredientsDisplay")
 
 // Fetches data (promise => data)
 async function fetchData(api_url) {
@@ -36,35 +53,36 @@ async function startup() {
     // Fetching dish
     const dish = await fetchData(api_url_dish).then(data => data[0]);
     const dish_ingredients = filterIngredients(dish);
-    postDish(dish);
+    // postDish(dish);
     console.log(dish_ingredients);
     console.log(dish)
 }
 
-// Filters measures and ingredients
-function filterIngredients(dish) {
-    return Object.keys(dish)
-        .filter(function (ingredient) {
-            return ingredient.startsWith("strIngredient") && dish[ingredient];
-        })
-        .map(key => {
-            return dish[key]
-        })
-        .sort();
-}
+// // Filters measures and ingredients
+// function filterIngredients(dish) {
+//     return Object.keys(dish)
+//         .filter(function (ingredient) {
+//             return ingredient.startsWith("strIngredient") && dish[ingredient];
+//         })
+//         .map(key => {
+//             return dish[key]
+//         })
+//         .sort();
+// }
 
-function postDish(dish) {
-    var nameEl = document.createElement("h1");
-    nameEl.innerHTML = dish.strMeal;
+// function postDish(dish) {
+//     var nameEl = document.createElement("h1");
+//     nameEl.innerHTML = dish.strMeal;
     
-    var imageEl = document.createElement("img");
-    imageEl.src = dish.strMealThumb;
-    imageEl.width = 400;
-    imageEl.height = 400;
+//     var imageEl = document.createElement("img");
+//     imageEl.src = dish.strMealThumb;
+//     imageEl.width = 400;
+//     imageEl.height = 400;
 
-    dishDisplayEl.appendChild(nameEl);
-    dishDisplayEl.appendChild(imageEl);
-}
+//     dishDisplayEl.appendChild(nameEl);
+//     dishDisplayEl.appendChild(imageEl);
+// }
+
 function postIngredients(ingredients) {
     ingredients.forEach(ingredient => {        
         let buttonEl = document.createElement("button");
@@ -75,5 +93,5 @@ function postIngredients(ingredients) {
     })
 }
 
-startup();
+// startup();
 
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+// API urls
+const api_url_dish = 'https://www.themealdb.com/api/json/v1/1/random.php';
+const api_url_ingredients = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
+
 // Importing packages
 const express = require("express");
 const path = require("path");
 const http = require("http");
 const socketio = require("socket.io");
+const { isPromise } = require("util/types");
 
 // Init server and socket
 const PORT = 3000;
@@ -10,18 +15,36 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// State variables
+var gameState = "";
+var gameDataFetched = false;
+
 // Set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Start server
+// Fetching data
+// Start server (only if we have loaded gamedata)
+const ingredients = fetchData(api_url_ingredients);
+const dish = fetchData(api_url_dish);
+
+// (async () => {
+//     ingredients.concat(await fetchData(api_url_ingredients));
+//     dish.concat(await fetchData(api_url_dish));
+// })
+
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+console.log(dish);
+console.log(ingredients);
+
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
 // Handle socket request from client
 const connections = [null, null];
 io.on("connection", socket => {
-    console.log(`New socket connection: ${socket}`);
+    console.log(`New socket connection:`);
 
-    // Find available player number
+    // Find available player spot
     let playerIndex = null;
     for (let i in connections) {
         if (!connections[i]) {
@@ -31,11 +54,44 @@ io.on("connection", socket => {
     }
     // Notify client about player number
     socket.emit("player-number", playerIndex);
-    console.log(`Player number #${playerIndex} connected`);
-
+    console.log(`Player number # ${playerIndex} connected`);
     // Ignore future connections if lobby full
     if (!playerIndex) return;
-    
     // Fill player space
     connections[playerIndex] = true;
+
+    // Send game data
+    socket.emit("game-data", ingredients, dish);
 });
+
+
+// Fetches data (promise => data)
+async function fetchData(api_url) {
+    return fetch(api_url)
+        .then(async (promise) => await promise.json())
+        .then((data) => data.meals)
+        .catch(error => console.log(error))
+}
+
+async function loadIngredients() {
+    return fetchData(api_url_ingredients);
+}
+async function loadDish() {
+    return fetchData(api_url_dish);
+}
+
+// // startup function
+// async function startup() {
+//     // Fetching ingredients
+//     const allIngredients = await fetchData(api_url_ingredients);
+//     postIngredients(allIngredients);
+//     console.log(allIngredients);
+//     // console.log(allIngredients.find(o => o.strIngredient == "Salmon"));
+
+//     // Fetching dish
+//     const dish = await fetchData(api_url_dish).then(data => data[0]);
+//     const dish_ingredients = filterIngredients(dish);
+//     postDish(dish);
+//     console.log(dish_ingredients);
+//     console.log(dish)
+// }
