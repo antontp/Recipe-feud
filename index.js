b81e33a9474c0f221e8ef3936a6e6997d3780052
Jeremiah Yee <jeremiah.yee@gmail.com>
2023-03-22 03:13:14 +0800

    chore: change server URL

--- index.js
+++ index.js
@@ -40,8 +40,7 @@ var timer = null;
 function startClient() {
   console.log(" * -- CLIENT STARTING -- * ");
   // socket = io("http://159.65.129.190:3000", { transports: ['websocket'] });
-  socket = io("http://localhost:3000/");
-  socket.emit("chat message", "hello world");
+  socket = io("https://nm2207.online");
   // Get game spot from server
   socket.once("game-spot", (spot) => {
     if (!spot) {
